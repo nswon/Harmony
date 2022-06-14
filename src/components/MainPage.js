b61ad6f90330d212078cbb6c7b63c082719408d4
nswon <swnam@gmail.com>
2022-06-14 15:52:30 +0900

    FEAT :: (#2) 이미지 오버 시 이미지 어두워짐

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -6,19 +6,26 @@ export default function Test01() {
 
         return (
             <div className='gallery'>
-                <Fade left>
-                    <img className='img01' src='./img01_N.png' />
-                    <img className='test01' src='./img02_N.png' />
-                </Fade>    
-                <Fade right>
-                    <img className='img02' src='./img02_N.png' />
-                </Fade>    
-                <Fade left>
-                    <img className='img03' src='./img03_N.png' />
-                </Fade>
-                <Slide right>
-                    <img className='img04' src='./img04_N.png' />
-                </Slide>                    
+                <a href='#' >
+                    <Fade left>
+                            <img className='img01' src='./img04_N.png' />
+                    </Fade>    
+                </a>
+                <a href='#' >
+                    <Zoom >
+                        <img className='img02' src='./img04_N.png' />
+                    </Zoom>    
+                </a>
+                <a href='#' >
+                    <Fade right>
+                        <img className='img03' src='./img04_N.png' />
+                    </Fade>    
+                    </a>
+                <a href='#' >
+                    <Zoom>
+                        <img className='img04' src='./img04_N.png' />
+                    </Zoom>                    
+                    </a>
             </div>
         );
 }
