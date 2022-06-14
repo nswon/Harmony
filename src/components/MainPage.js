c4e8aab49c63753dcc31526861a282363ddba00b
nswon <swnam@gmail.com>
2022-06-14 14:52:46 +0900

    FEAT :: (#1) 스크롤 내릴 시 Fade

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,19 +1,24 @@
 import { render } from '@testing-library/react';
 import React from 'react'
-import { Fade, Zoom } from 'react-reveal';
+import { Fade, Slide, Zoom } from 'react-reveal';
 
 export default function Test01() {
 
         return (
             <div className='gallery'>
-                <Fade>
-                    <img className='img01' src='./img01.png' />
-                    <img className='img03' src='./img03.png' />
-                    <img className='img04' src='./img04.png' />
-                    <img className='img05' src='./img05.png' />
-                    hello
-                    dffad
+                <Fade left>
+                    <img className='img01' src='./img01_N.png' />
+                    <img className='test01' src='./img02_N.png' />
+                </Fade>    
+                <Fade right>
+                    <img className='img02' src='./img02_N.png' />
+                </Fade>    
+                <Fade left>
+                    <img className='img03' src='./img03_N.png' />
                 </Fade>
+                <Slide right>
+                    <img className='img04' src='./img04_N.png' />
+                </Slide>                    
             </div>
         );
 }
