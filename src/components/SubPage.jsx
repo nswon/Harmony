4a9039e9151597e8e4e868f8d104bdead8a5be11
root 5856 <whehdrms6810@naver.com>
2022-06-28 16:48:46 +0900

    2022-06-28

--- src/components/SubPage.jsx
+++ src/components/SubPage.jsx
@@ -4,11 +4,15 @@ import "../App_L.css";
 export default function SubPage() {
   const seoul = "se\noul";
   return (
-    <div>
+    <div className="map">
+      <div className="back"></div>
+      <div className="dark"></div>
       <div className="seoul">
-        <p id="seoul">{seoul}</p>
-        <div>march 2009</div>
+        <p className="seoul_p">{seoul}</p>
       </div>
+      <div className="seoulLine"></div>
+      {/* seoul */}
+      <div className="redDot"></div>
     </div>
   );
 }
